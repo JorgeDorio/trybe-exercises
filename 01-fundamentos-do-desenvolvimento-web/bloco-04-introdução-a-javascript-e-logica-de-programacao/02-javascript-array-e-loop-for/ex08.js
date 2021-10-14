c8abf38f7dd8f2594266584da612632e9ac75f2c
Jorge Dorio <jorgegabrielbd.jj@gmail.com>
2021-10-14 15:03:49 -0400

    update

---
+++ 01-fundamentos-do-desenvolvimento-web/bloco-04-introdução-a-javascript-e-logica-de-programacao/02-javascript-array-e-loop-for/ex08.js
@@ -0,0 +1,7 @@
+let a = []
+
+for (i = 1; i <= 25; i++) {
+  a.push(i)
+}
+
+console.log(a)
---
+++ 01-fundamentos-do-desenvolvimento-web/bloco-04-introdução-a-javascript-e-logica-de-programacao/02-javascript-array-e-loop-for/ex09.js
@@ -0,0 +1,9 @@
+let a = []
+
+for (i = 1; i <= 25; i++) {
+  a.push(i)
+}
+
+for (i = 0; i < 25; i++) {
+  console.log(a[i] / 2)
+}
